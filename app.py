4aa3a4c598a84d95f8144493c05cf5132ec622f1
EC2 Default User <ec2-user@ip-172-31-6-174.ap-south-1.compute.internal>
2020-12-29 04:34:07 +0000

    version 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1><center>This is a Demo Flaskapp version1 Application</center></h1>'
+  return '<h1><center>This is a Demo Flaskapp version2 Application</center></h1>'
 
 app.run(host='0.0.0.0', port=5000)
 
